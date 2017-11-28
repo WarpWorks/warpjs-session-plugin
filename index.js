de0593985713a062397e67dea6bdab90e51e9da5
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-11-28 13:23:35 -0500

    Initial refactoring with permissions

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const app = require('./server/app');
+const canEdit = require('./lib/can-edit');
 const middlewares = require('./server/middlewares');
 
 function plugin(config, warpCore, Persistence) {
     return (baseUrl, staticUrl) => app(config, warpCore, Persistence, baseUrl, staticUrl);
 }
 
-plugin.middlewares = middlewares;
+plugin.middlewares = (config, warpCore, Persistence) => middlewares(config, warpCore, Persistence);
+plugin.canEdit = (config, persistence, entity, instance, user) => canEdit(config, persistence, entity, instance, user);
 
 module.exports = plugin;
--- index.unit.test.js
+++ index.unit.test.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const testHelpers = require('@quoin/node-test-helpers');
 
 const moduleToTest = require('./index');
@@ -5,7 +6,49 @@ const moduleToTest = require('./index');
 const expect = testHelpers.expect;
 
 describe("index", () => {
-    it("should expose a function with 3 params", () => {
-        expect(moduleToTest).to.be.a('function').to.have.lengthOf(3);
+    const clone = _.clone(moduleToTest);
+
+    it("should export a function", () => {
+        expect(moduleToTest).to.be.a('function');
+    });
+
+    context("function", () => {
+        it("should take 3 params", () => {
+            expect(moduleToTest).to.have.lengthOf(3);
+        });
+    });
+
+    context(".middlewares()", () => {
+        it("should expose .middlewares", () => {
+            expect(moduleToTest).to.have.property('middlewares');
+        });
+
+        it("should be a function with 3 params", () => {
+            expect(moduleToTest.middlewares).to.be.a('function').and.to.have.lengthOf(3);
+        });
+
+        after(() => {
+            delete clone.middlewares;
+        });
+    });
+
+    context(".canEdit()", () => {
+        it("should expose .canEdit", () => {
+            expect(moduleToTest).to.have.property('canEdit');
+        });
+
+        it("should be a function with 5 params", () => {
+            expect(moduleToTest.canEdit).to.be.a('function').and.to.have.lengthOf(5);
+        });
+
+        after(() => {
+            delete clone.canEdit;
+        });
+    });
+
+    describe("after all properties", () => {
+        it("should be all tested", () => {
+            expect(clone).to.be.empty();
+        });
     });
 });
